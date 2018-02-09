78ec9c01beac281a561d2ec02bda7eb4412df4f6
Austin Stevens <austinestevens@outlook.com>
2018-02-09 10:10:01 -0500

    Initial commit. No functionality implemented.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Switch, Link, Route } from 'react-router-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const App = () => (
+  <div>
+    <header>
+      <h1>Not Your Parents's Rolodex</h1>
+    </header>
+  </div>
+)
+
+
+
+ReactDOM.render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('root'))
