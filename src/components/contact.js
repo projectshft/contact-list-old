7fdb35bd5fb1a849c6c54e40087db22b5f379329
Lindsay Schinasi <lindsay.schinasi@gmail.com>
2018-08-27 02:48:17 -0400

    Fix contact.js bug so that titles can render properly

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,23 +5,19 @@ import ReactDOM from 'react-dom';
 import Contacts from './Contacts'
 import ContactsList from './contacts_list';
 
-class SingleContact extends Component {
-  constructor(props) {
-    super(props);
-    console.log('properties passed to contact are ', this.props)
-    const id = Number(this.props.match.params.id);
+const SingleContact = (props) =>{
 
-  }
+  const singleContact = _.find(props.contacts, { id: parseInt(props.props.match.params.id, 10) });
 
+  console.log(singleContact);
 
-render(){
-  return (
+return (
     <div>
-      <h1> helllooooo </h1>
-      <h1> {this.props.name}</h1>
+      <h1> {singleContact.name} </h1>
+
     </div>
   )
-}
+
 }
 
 export default SingleContact
