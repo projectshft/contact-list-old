60869350592fb9a6b7f2e41310c9a2a406c4747d
Marcus Swain <marcusswain@Marcuss-MacBook-Pro.local>
2018-11-30 11:43:19 -0500

    initial commit

---
+++ src/Components/DeleteUser.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class DeleteUser extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Are You Sure you want to Delete This User?</h1>
+      </div>
+    )
+  }
+}
+
+export default DeleteUser
---
+++ src/Components/NewUser.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class NewUser extends React.Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default NewUser
