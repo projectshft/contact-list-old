a0298793b5305db521be5241e431f9bed04e4fa2
Betty <bmay.wanjiru@gmail.com>
2018-11-30 11:01:19 -0500

    Created and rendered a basic home page. Added new files to the components folder. They are currently empty

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Home = () => (
+  <div>
+    <h1>Contacts List App</h1>
+  </div>
+);
+
+export default Home
+
+{/* On this Page (the home page), their should be a current list of contacts and an add contact button that should re-direct you to another page to do so*/}
