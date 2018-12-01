183d0ca04c4b45dd1abc335f69059bb513670998
Alicia Hetrick <ahetrick22@gmail.com>
2018-12-01 15:47:06 -0500

    fixed bug with delete modal where it always selected the last contact to delete

--- src/components/ContactListItem.js
+++ src/components/ContactListItem.js
@@ -1,38 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { sendEvent, getState } from "../state";
-import Modal from "react-modal";
-
-Modal.setAppElement("#root");
+import { sendEvent } from "../state";
 
 const ContactListItem = ({ contact }) => {
   const contactURL = `/contacts/${contact.id}`;
 
   return (
-    <div className="card col-2">
+    <div className="card col-2" >
       <img className="contact-image-main" alt={contact.name} src={contact.image_url}/>
       <Link to={contactURL} onClick={() => sendEvent("setSelectedContact", contact)}
       className = "contact-name-main">
         {contact.name}
       </Link>
-      <button type="button" className="btn btn-danger btn-sm" onClick={() => sendEvent("requestDelete")}>
+      <button type="button" className="btn btn-danger btn-sm" onClick={() => {sendEvent('setSelectedContact', contact); sendEvent('requestDelete')}}>
         Delete Contact
       </button>
-      <Modal
-          className = "delete-modal"
-          overlayClassName="overlay"
-           closeTimeoutMS={250}
-           isOpen={getState('showDeleteModal')}
-           contentLabel="onRequestClose"
-           onRequestClose={this.cancelDelete}>
-          <p>Are you sure you wish to delete {contact.name}?</p>
-          <button className="btn btn-success modal-btn" onClick={() => sendEvent("deleteContact", contact)}>
-            Confirm Delete
-          </button>
-          <button className="btn btn-danger modal-btn" onClick={() => sendEvent('cancelDelete')}>
-            Never Mind
-          </button>
-      </Modal>
     </div>
   );
 };
--- src/components/FullContactList.js
+++ src/components/FullContactList.js
@@ -1,12 +1,44 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom'
-import { getState } from '../state';
+import { getState, sendEvent } from '../state';
 import ContactListItem from './ContactListItem';
+import Modal from "react-modal";
+
+Modal.setAppElement('#root');
 
 const FullContactList = () => {
   //forces a regeneration of the contact list whenever the list renders
   const newContactList = () =>
     getState('contacts').map((c) => <ContactListItem key={c.id} contact={c} />)
+
+    //renders the modal only if the user has clicked a delete button
+    const renderModal = () => {
+      let contactToDelete;
+      if (getState('showDeleteModal')) {
+        contactToDelete = getState('selectedContact');
+      }
+      if (contactToDelete) {
+        return (
+          <Modal
+                className = "delete-modal"
+                overlayClassName="overlay"
+                 closeTimeoutMS={250}
+                 isOpen={getState('showDeleteModal')}
+                 contentLabel="onRequestClose"
+                 onRequestClose={this.cancelDelete}>
+                <p>Are you sure you wish to delete {contactToDelete.name}?</p>
+                <button className="btn btn-success modal-btn" onClick={() => sendEvent("deleteContact", contactToDelete)}>
+                  Confirm Delete
+                </button>
+                <button className="btn btn-danger modal-btn" onClick={() => sendEvent('cancelDelete')}>
+                  Never Mind
+                </button>
+            </Modal>
+          )}
+          else {
+            return null;
+          }
+        }
 
   return (
       <Fragment>
@@ -17,6 +49,7 @@ const FullContactList = () => {
         <div className="add-new-btn">
         <Link to="/contacts/new" className="btn btn-primary btn-lg add-btn">Add New Contact</Link>
         </div>
+        {renderModal()}
       </Fragment>
     );
 }
