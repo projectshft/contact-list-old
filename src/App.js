aa19593152af3b87932544478bbccf94f965a6be
James DeVore <jdevore4592@gmail.com>
2018-11-30 19:53:24 -0500

    Added add contact functionality

--- src/App.js
+++ src/App.js
@@ -13,17 +13,32 @@ constructor(){
 
   this.state = {
     contacts: [
-      { firstName: "Diego", lastName: "Lee", email: 'diégo.lee@example.com', phone: '(349)-329-9253', imgUrl: "https://randomuser.me/api/portraits/men/40.jpg", key: Math.round(Math.random() * 100000000) },
-      { firstName: "Sarah", lastName: "Connor", email: 'ThymeTraveler@t1000.com', phone: '(102)-619-1984', imgUrl: "https://randomuser.me/api/portraits/women/58.jpg", key: Math.round(Math.random() * 100000000) },
-      { firstName: "Chris P", lastName: "Bacon", email: 'whatsCookin@goodLookin.com', phone: '(555)-552-2266', imgUrl: "https://randomuser.me/api/portraits/men/32.jpg", key: Math.round(Math.random() * 100000000) },
-      { firstName: "Sally", lastName: "Sugar", email: 'ThymeTraveler@t1000.com', phone: '(102)-619-1984', imgUrl: "https://randomuser.me/api/portraits/women/22.jpg", key: Math.round(Math.random() * 100000000) },
-      { firstName: "Sarah", lastName: "", email: 'ThymeTraveler@t1000.com', phone: '(102)-619-1984', imgUrl: "https://randomuser.me/api/portraits/women/52.jpg", key: Math.round(Math.random() * 100000000) },
+      {firstName: "Diego", lastName: "Lee", email: 'diégo.lee@example.com', phone: '(349)-329-9253', 
+      imgUrl: "https://randomuser.me/api/portraits/men/40.jpg", key: Math.round(Math.random() * 100000000) },
+
+      {firstName: "Sarah", lastName: "Connor", email: 'ThymeTraveler@t1000.com', phone: '(102)-619-1984', 
+      imgUrl: "https://randomuser.me/api/portraits/women/58.jpg", key: Math.round(Math.random() * 100000000) },
+
+      {firstName: "Chris P", lastName: "Bacon", email: 'whatsCookin@goodLookin.com', phone: '(555)-552-2266', 
+      imgUrl: "https://randomuser.me/api/portraits/men/32.jpg", key: Math.round(Math.random() * 100000000) },
+
+      {firstName: "Sally", lastName: "Sugar", email: 'ThymeTraveler@t1000.com', phone: '(102)-619-1984', 
+      imgUrl: "https://randomuser.me/api/portraits/women/22.jpg", key: Math.round(Math.random() * 100000000) },
+
+      {firstName: "Sarah", lastName: "", email: 'ThymeTraveler@t1000.com', phone: '(102)-619-1984', 
+      imgUrl: "https://randomuser.me/api/portraits/women/52.jpg", key: Math.round(Math.random() * 100000000) },
+      {firstName: "James", lastName: "Bond", email: 'whatsCookin@goodLookin.com', phone: '(555)-552-2266', 
+      imgUrl: "https://randomuser.me/api/portraits/men/4.jpg", key: Math.round(Math.random() * 100000000) },
+
 
     ],
-
+    
   };
- 
+  this.addContact = this.addContact.bind(this)
 
+  }
+    addContact(contactInfo) {
+    this.setState({ contacts: this.state.contacts.concat([contactInfo]) })
 }
 
  
@@ -33,7 +48,7 @@ constructor(){
     return <div className="container">
       
       <Switch>
-        <Route exact path='/contacts' render={() => <ContactList contacts={this.state.contacts} />} />
+        <Route exact path='/contacts' render={() => <ContactList contacts={this.state.contacts} addContact={this.addContact} />} />
         <Route path='/contacts/:id' render={(props) => <Info getPersonById={this.getPersonById} {...props} contacts={contacts} />} />
 
       </Switch>
--- src/components/contact-list.js
+++ src/components/contact-list.js
@@ -1,19 +1,84 @@
-import React from 'react'
+import React, { Component } from "react";
 import ContactItem from './contactItem'
-import {Route} from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 
-const ContactList = ({contacts}) => {
- 
-  const contactList = contacts.map(person => (
-    <ContactItem key={contacts.key} contact={person} />
-  ))
-  return (
-    <div className="row">
+class ContactList extends Component {
+  constructor(props) {
+    super()
+
+    this.state = {
+      firstName: '',
+      lastName: '',
+      phone: '',
+      email: '',
+      imgUrl: '',
       
-    {contactList}  
-    </div>
-  )
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+  
+  contactList = () => this.props.contacts.map(person => (
+    <ContactItem key={this.props.contacts.key} contact={person} />
+  ))
+
+  handleClick () {
+    let contactInfo = {
+      firstName:this.state.firstName,
+      lastName:this.state.lastName,
+      phone:this.state.phone,
+      email:this.state.email,
+      imgUrl:this.state.imgUrl,
+      key: Math.round(Math.random() * 100000000)
+    }
+    this.props.addContact(contactInfo)
+  }
+ 
+
+  render() {
+    return <div>
+      <div className="row">{this.contactList(this.contacts)}</div>
+      <div className="row add-contact">
+        <div className="col-md-9">
+          <label>First Name</label>
+          <input type="text" className="form-control" value={this.state.firstName} onChange={event => this.setState(
+            { firstName: event.target.value }
+          )} />
+        </div>
+
+        <div className="col-md-9">
+          <label>Last Name</label>
+          <input type="text" className="form-control" value={this.state.lastName} onChange={event => this.setState(
+            { lastName: event.target.value }
+          )} />
+        </div>
+        <div className="col-md-9">
+          <label>Email</label>
+          <input type="text" className="form-control" value={this.state.email} onChange={event => this.setState(
+            { email: event.target.value }
+          )} />
+        </div>
+        <div className="col-md-9">
+          <label>Phone #</label>
+          <input type="text" className="form-control" value={this.state.phone} onChange={event => this.setState(
+            { phone: event.target.value }
+          )} />
+        </div>
+        <div className="col-md-9">
+          <label>Pic URL</label>
+          <input type="text" className="form-control" value={this.state.imgUrl} onChange={event => this.setState(
+            { imgUrl: event.target.value }
+          )} />
+          <br />
+        </div>
+      </div>
+      <div className="row">
+        <button onClick={this.handleClick} className="btn btn-confirm col-md-12 add">
+          Add Contact
+          </button>
+      </div>
+    </div>;
+  }
 }
 
 export default ContactList
