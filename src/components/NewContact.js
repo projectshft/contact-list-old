94cfedcf68418e0bfc40105609fce3925c2a638d
Edward Barton <edward.barton@icloud.com>
2018-11-30 20:17:51 -0500

    Setup new contact form page

--- src/components/NewContact.js
+++ src/components/NewContact.js
@@ -1,7 +1,67 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const NewContact = () => (
-  <h1>New Contact Form Page</h1>
-);
+class NewContact extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      email: '',
+      phone: '',
+      image: ''
+    };
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+  
+  handleSubmit = e => {
+    e.preventDefault();
+    
+    const newContact = {
+      name: this.state.name,
+      email: this.state.email,
+      phone: this.state.phone,
+      image: this.state.image
+    };
+
+    this.props.addContact(newContact);
+    window.location.assign(`/contacts`);
+  }
+
+  render() {
+    return (
+      <div className="container my-5">
+        <div className="row">
+          <div className="col-md-4 offset-md-4">
+            <form className="border rounded p-5" onSubmit={this.handleSubmit}>
+              <h5 className="mb-5 text-center">New Contact</h5>
+              <div className="form-group">
+                <label htmlFor="name">Name</label>
+                <input type="text" value={this.state.name} name='name' onChange={this.handleChange} className="form-control" id="name" placeholder="Enter name" autoFocus required />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email Address</label>
+                <input type="email" value={this.state.email} name='email' onChange={this.handleChange} className="form-control" id="email" placeholder="Enter email" required />
+              </div>
+              <div className="form-group">
+                <label htmlFor="phone">Phone</label>
+                <input type="tel" value={this.state.phone} name='phone' onChange={this.handleChange} className="form-control" id="phone" placeholder="Enter phone" required />
+              </div>
+              <div className="form-group">
+                <label htmlFor="image">Image</label>
+                <input type="url" value={this.state.image} name='image' onChange={this.handleChange} className="form-control" id="image" placeholder="Enter image URL" required />
+              </div>
+              <button type="submit" className="btn btn-primary">Save</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
 
 export default NewContact;
