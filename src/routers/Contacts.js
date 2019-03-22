1ee5865c0c73ec85dcb7e9c70c150a6ae753205a
Jason Doss <doss2k@gmail.com>
2019-03-22 10:48:49 -0400

    Add router switches, import statements, and example contact

--- src/routers/Contacts.js
+++ src/routers/Contacts.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import ContactsList from '../components/contactsList';
+import AddContact from '../components/addContact';
+import ContactInfo from '../components/contactInfo';
 
 class Contacts extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      contacts: []
+      contacts: [
+        {
+          "id": 70219577,
+          "lastName": "Einstein",
+          "firstName": "Albert",
+          "email": "aeinstein@example.com",
+          "phone": "15555555555",
+          "imageURL": "https://en.wikipedia.org/wiki/Albert_Einstein#/media/File:Einstein_1921_by_F_Schmutzer_-_restoration.jpg"
+        }
+      ]
     };
   }
 
   render () {
-    return (<h1>Contact List</h1>);
+    return (
+      <Switch>
+          <Route exact path='/contacts' render={() => (
+            <ContactsList contacts={this.state.contacts} /> 
+          )} />
+          <Route path='/contacts/new' render={props => (
+            <AddContact routerProps={props} addContact={this.addContact} />
+          )} />
+          <Route path='/contacts/:id' render={props => (
+            <ContactInfo routerProps={props} contacts={this.state.contacts} />
+          )} />
+        </Switch>
+    );
   }
 }
 
