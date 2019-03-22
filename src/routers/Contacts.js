99e8c32145693f508cdc874fe402bb93ccc6443a
Jason Doss <doss2k@gmail.com>
2019-03-22 13:56:56 -0400

    Add function to add a new contact, second default contact, fixed route paths

--- src/routers/Contacts.js
+++ src/routers/Contacts.js
@@ -16,25 +16,41 @@ class Contacts extends React.Component {
           "firstName": "Albert",
           "email": "aeinstein@example.com",
           "phone": "15555555555",
-          "imageURL": "https://en.wikipedia.org/wiki/Albert_Einstein#/media/File:Einstein_1921_by_F_Schmutzer_-_restoration.jpg"
+          "imageURL": "https://upload.wikimedia.org/wikipedia/en/8/86/Einstein_tongue.jpg"
+        },
+        {
+          "id": 70219566,
+          "lastName": "DJ",
+          "firstName": "Marshmello",
+          "email": "marshmello@sickbeats.com",
+          "phone": "123456789",
+          "imageURL": "https://pbs.twimg.com/profile_images/1102011359481389056/EKK_BaDW_400x400.jpg"
         }
-      ]
+      ]     
     };
+
+    this.addContact = this.addContact.bind(this);
   }
+
+  addContact (newContact) {
+    this.setState({contacts: this.state.contacts.concat([newContact])});
+  }
+
+  // Sets up routes to each different component and passes it the props it requires
 
   render () {
     return (
       <Switch>
-          <Route exact path='/contacts' render={() => (
-            <ContactsList contacts={this.state.contacts} /> 
-          )} />
-          <Route path='/contacts/new' render={props => (
-            <AddContact routerProps={props} addContact={this.addContact} />
-          )} />
-          <Route path='/contacts/:id' render={props => (
-            <ContactInfo routerProps={props} contacts={this.state.contacts} />
-          )} />
-        </Switch>
+        <Route exact path='/contacts' render={() => (
+          <ContactsList contacts={this.state.contacts} /> 
+        )} />
+        <Route path='/contacts/new' render={props => (
+          <AddContact addContact={this.addContact} routerProps={props} />
+        )} />
+        <Route path='/contacts/:id' render={props => (
+          <ContactInfo contacts={this.state.contacts} routerProps={props} />
+        )} />
+      </Switch>
     );
   }
 }
