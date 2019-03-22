603caaec9933b2ca25d856fa0e06b7a5d48ef10b
Jason Doss <doss2k@gmail.com>
2019-03-22 11:44:14 -0400

    Add ability to find clicked contact and display that contacts info

--- src/components/contactInfo.js
+++ src/components/contactInfo.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import _ from 'lodash';
 
-const ContactInfo = ({contacts}) => (
-  <div>
-    <h1>Contact Info</h1>
-  </div>
-);
+// This component displays one individuals full contact info
+
+const ContactInfo = ({contacts, routerProps}) => {
+  const contact = _.find(contacts, { id: parseInt(routerProps.match.params.id, 10) });
+
+  if (!contact) {
+    return <div>Sorry, but the contact was not found</div>
+  }
+
+  return (
+    <div>
+      <h3>{contact.lastName}, {contact.firstName}</h3>
+      <img src={contact.imageURL}></img>
+      <h5>Email: {contact.email}</h5>
+      <h5>Phone: {contact.phone}</h5>
+      <h5>Image: {contact.imageURL}</h5>
+      <Link to='/contacts'>Back</Link>
+    </div>
+  )
+}
 
 export default ContactInfo;
