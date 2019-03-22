ae94587e49cb8cebe269b1e7050352848d391577
Jason Doss <doss2k@gmail.com>
2019-03-22 13:57:26 -0400

    Add comments

--- src/routers/App.js
+++ src/routers/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom';
 import Contacts from './Contacts';
 
+// On page load redirects user to 'root'/contacts as starting view
+
 const App = () => (
   <Switch>
     <Route exact path='/'>
