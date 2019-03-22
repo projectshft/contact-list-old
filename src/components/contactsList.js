7edf76176c9a786d4464a338f1cf80b6f24688cd
Jason Doss <doss2k@gmail.com>
2019-03-22 11:12:57 -0400

    Add ability to map over contacts and display list of current contacts

--- src/components/contactsList.js
+++ src/components/contactsList.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
+/* This component takes in the contacts state, maps through each one, and displays a list of the
+   current contacts in your list */
 
 const ContactsList = ({contacts}) => (
   <div>
-    <h1>Contact List</h1>
+    <h3>My Contacts List:</h3>
+    <ul>
+      { contacts.map(contact => (
+          <li key={contact.id}>
+            <Link to={`/roster/${contact.id}`}>{contact.lastName}, {contact.firstName}</Link>
+          </li>
+        ))
+      }
+    </ul>
   </div>
 );
 
