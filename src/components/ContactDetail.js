389f28359f53188f951afbb99b8e30d2a3c48752
mcstreilein <madelinestreilein@Madelines-MacBook-Air.local>
2019-03-23 18:03:52 -0400

    deleting ContactDetail2 and ContactDetail 3

--- src/components/ContactDetail.js
+++ src/components/ContactDetail.js
@@ -2,30 +2,37 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const ContactDetail = props => {
-    const user = props.match.params.contactId;
+    const clickedContact = props.match.params.contactId;
     console.log(props.contacts[1].email);
-
+    
+    function isContact(clickedContact) {
+        return clickedContact === props.contacts.id;
+    }
+    console.log(isContact); //true
+    console.log(props.contacts.find(isContact)); //undefined
     // isContact = c => c.id === id;
-    //     this.contacts.find(isContact);
+    //     props.contacts.find(isContact);
         
-    if (!user) {
+    if (!clickedContact) { //this doesn't work
         return <div>There is not a record of this contact.<Link to="/contacts"><button className="btn-dark btn btn-sm">Back to Contacts</button></Link></div>;
     } else {
         return (
-            <div className="col-md-12 offset-md-2">
-                <div className="row">
-                    <div className="col-md-3">
-                        <img className="img-responsive contact-photo" style={contactPhotoStyle} src={props.contacts[1].image_url} alt="contact"/>
-                    </div>
-                    <div className="col-md-3">
-                        <p>{props.contacts[1].fname} {props.contacts[1].lname}</p>
-                        <p>{props.contacts[1].phone_number}</p>
-                        <p>{props.contacts[1].email}</p>
-                        <p>{props.match.params.contactId}</p>
-                    </div>
+            <div className="container">
+                <div className="col-md-12 offset-md-2">
+                    <div className="row">
+                        <div className="col-md-3">
+                            <img className="img-responsive contact-photo" style={contactPhotoStyle} src={props.contacts[1].image_url} alt="contact"/>
+                        </div>
+                        <div className="col-md-3 offset-md-1">
+                            <p>{props.contacts[1].fname} {props.contacts[1].lname}</p>
+                            <p>{props.contacts[1].phone_number}</p>
+                            <p>{props.contacts[1].email}</p>
+                            <p>{props.match.params.contactId}</p>
+                        </div>
 
-                    {/* a "Back" button will takes the user back to the /contacts view and main contacts list. */}
-                    <Link to="/contacts"><button className="btn-dark btn btn-sm">Back to Contacts</button></Link>
+                        {/* a "Back" button will takes the user back to the /contacts view and main contacts list. */}
+                        <Link to="/contacts"><button className="btn-dark btn btn-sm">Back to Contacts</button></Link>
+                    </div>
                 </div>
             </div>
         )
