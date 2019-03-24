f68979dab4c06c9b0778cbe85af5bfa05ee622d6
Jason Doss <doss2k@gmail.com>
2019-03-24 11:25:39 -0400

    Import custom css file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
