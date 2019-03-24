a100d853eec1364a756b8565a83fc0d464c8819a
Jason Doss <doss2k@gmail.com>
2019-03-24 16:33:35 -0400

    Add editContact function and route for page

--- src/routers/Contacts.js
+++ src/routers/Contacts.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import ContactsList from '../components/contactsList';
 import AddContact from '../components/addContact';
 import ContactInfo from '../components/contactInfo';
+import EditContact from '../components/editContact';
 
 /* This component handles the main state of the app, any functions that change the apps state, along with
    the routes for the main page views. */
@@ -26,7 +27,7 @@ class Contacts extends React.Component {
           "lastName": "DJ",
           "firstName": "Marshmello",
           "email": "marshmello@sickbeats.com",
-          "phone": "123456789",
+          "phone": "(456)-690-5460",
           "imageURL": "https://pbs.twimg.com/profile_images/1102011359481389056/EKK_BaDW_400x400.jpg"
         },
         {
@@ -42,6 +43,7 @@ class Contacts extends React.Component {
 
     this.addContact = this.addContact.bind(this);
     this.deleteContact = this.deleteContact.bind(this);
+    this.editContact = this.editContact.bind(this);
   }
 
   // Concats a new contact array with the current contacts array and sets the current state to the new array
@@ -50,7 +52,7 @@ class Contacts extends React.Component {
     this.setState({contacts: this.state.contacts.concat([newContact])});
   }
 
-  /* Firt confirms if the user wants to delete the contact, then filters out all contacts that do not 
+  /* First confirms if the user wants to delete the contact, then filters out all contacts that do not 
      match the ID of the contact to delete and then sets the current state to the resulting array */
 
   deleteContact (deleteContact) {
@@ -58,6 +60,24 @@ class Contacts extends React.Component {
     if (confirmDelete) {
       this.setState({contacts: this.state.contacts.filter(item => item.id !== deleteContact.id)});
     }
+  }
+
+  editContact (editContact) {
+    this.setState({
+      contacts: this.state.contacts.map(item => {
+        if(item.id === editContact.id) { 
+          item = {
+            id: editContact.id,
+            firstName: editContact.firstName,
+            lastName: editContact.lastName,
+            email: editContact.email,
+            phone: editContact.phone,
+            imageURL: editContact.imageURL
+          }
+        }
+        return item;
+      })
+    });
   }
 
   // Sets up routes to each different component and passes it the props it requires
@@ -71,9 +91,12 @@ class Contacts extends React.Component {
         <Route path='/contacts/new' render={(props) => (
           <AddContact addContact={this.addContact} routerProps={props} />
         )} />
+        <Route path='/contacts/:id/edit' render={(props) => (
+          <EditContact contacts={this.state.contacts} editContact={this.editContact} routerProps={props} />
+        )} />
         <Route path='/contacts/:id' render={(props) => (
           <ContactInfo contacts={this.state.contacts} routerProps={props} />
-        )} />
+        )} />        
       </Switch>
     );
   }
