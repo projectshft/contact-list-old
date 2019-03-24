48701cf0d8e4bc69bdbdb99a3a1fe8f743ce30d8
Jason Doss <doss2k@gmail.com>
2019-03-24 08:05:58 -0400

    Add contact info to bootstrap card for better looks, add back to contacts link on error view

--- src/components/contactInfo.js
+++ src/components/contactInfo.js
@@ -8,17 +8,33 @@ const ContactInfo = ({contacts, routerProps}) => {
   const contact = _.find(contacts, { id: parseInt(routerProps.match.params.id, 10) });
 
   if (!contact) {
-    return <div>Sorry, but the contact was not found</div>
+    return (
+      <div>
+        <h3>Sorry, but the contact was not found</h3>
+        <Link to='/contacts'>Back To Contact List</Link>
+      </div>
+    )
   }
 
   return (
     <div>
-      <h3>{contact.firstName} {contact.lastName}</h3>
-      <img src={contact.imageURL}></img>
-      <h5>Email: {contact.email}</h5>
-      <h5>Phone: {contact.phone}</h5>
-      <h5>Image: {contact.imageURL}</h5>
-      <Link to='/contacts'>Back</Link>
+      <div className="card mb-3 shadow rounded" style={{maxWidth: 540, margin: 40, background: '#eee', border: '1.5px solid black'}}>
+        <div className="row no-gutters">
+          <div className="col-md-4">
+            <img src={contact.imageURL} className="card-img" alt="User"></img>
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h3 className="card-title">{contact.firstName} {contact.lastName}</h3>
+              <p className="card-text">Phone: {contact.phone}</p>
+              <p className="card-text">Email: {contact.email}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div style={{marginLeft: 40}}>
+      <Link to='/contacts'>Back To Contact List</Link>
+      </div>
     </div>
   )
 }
