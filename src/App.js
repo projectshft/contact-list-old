d6f9d9a779a826c7b6ae8c98810f1b5fb0b8b484
Luc Saunders <lucsaunders@gmail.com>
2019-03-24 22:11:20 -0400

    Update AddContactForm and add basic functionality.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react';
 
 import ContactList from './components/ContactList';
 import Header from './components/Header';
-// import AddContact from './components/AddContact';
+import AddContactForm from './components/AddContactForm';
 // import EditContact from './components/EditContact';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -16,6 +16,7 @@ class App extends Component {
       <div className="App">
         <Header branding="PS Contact List" />
         <div className="container">
+          <AddContactForm />
           <ContactList />
         </div>
       </div>
@@ -24,37 +25,3 @@ class App extends Component {
 }
 
 export default App;
-
-// import React, { Component } from 'react';
-// // import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import Contact from './components/Contact';
-// import ContactList from './components/ContactList';
-// import Header from './components/Header';
-// import AddContact from './components/AddContact';
-// import EditContact from './components/EditContact';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './App.css';
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <Header psTitle="PS Contact List" />
-//         <Contact
-//           name="Charles Mingus"
-//           image_url="#"
-//           email="mingus@gmail.com"
-//           phone="1-800-fat-bass"
-//         />
-//         <Contact
-//           name="Patty Smith"
-//           image_url="#"
-//           email="PattySmith@gmail.com"
-//           phone="1-800-hip-poet"
-//         />
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
