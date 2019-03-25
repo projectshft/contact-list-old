c3653d04c95d2e1e90f05d9d399adb06e04fb53e
Richard Eldridge <eldridge.rich@gmail.com>
2019-03-24 20:59:36 -0400

    fixed import statement for uuid generator

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import uuid from '../../node_modules/uuid';
+import uuid from 'uuid';
 
 class AddContact extends Component {
   state = {
