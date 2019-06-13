f0c3014613c9a9804fb3cf5d4b4f682dc6f92575
Luc Saunders <lucsaunders@gmail.com>
2019-06-13 19:56:53 -0400

    Add About page. Add 404 Not Found page.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ContactList from './components/contacts/ContactList';
 import AddContact from './components/contacts/AddContact';
 import Header from './components/layout/Header';
-// import About from './components/pages/About';
-// import NotFound from './components/pages/NotFound';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 import { Provider } from './context';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -21,8 +21,8 @@ class App extends Component {
               <Switch>
                 <Route exact path="/" component={ContactList} />
                 <Route exact path="/contact/add" component={AddContact} />
-                {/* <Route exact path="/about" component={About} />
-                <Route component={NotFound} /> */}
+                <Route exact path="/about" component={About} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
