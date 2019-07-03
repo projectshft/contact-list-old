e614a9caed0b9b9b2d425767dba669546835cdb1
dposse <dposse.125@gmail.com>
2019-07-03 14:33:35 -0400

    Moved header to Header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const Header = () => (
+  <div className='container'>
+    <div className='row'>
+      <h1>Contacts</h1>
+      <button className='btn btn-primary'>Add Contact</button>
+    </div>
+  </div>
+);
+
+export default Header;
