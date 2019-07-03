8807a49e8da18bd3d6663dda2dcfad6941a71895
Jake Galligan <galligan1996@gmail.com>
2019-07-03 18:54:55 -0400

    Setting basic class layout for Contacts Class

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -1,1 +1,26 @@
 import React, { Component } from 'react';
+
+
+class Contacts extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      contactList = {[
+        {
+          'id': 1
+          'name': 'Sir PumperNickle Junior'
+          'image_url': 'https://pixel.nymag.com/imgs/daily/vulture/2017/11/27/27-lil-pump.w700.h700.jpg'
+          'email': 'lilPump@gmail.com'
+          'phone_number': '305-274-2834'
+        }
+
+      ]}
+    }
+  }
+
+
+  render() {
+    return ()
+  }
+}
