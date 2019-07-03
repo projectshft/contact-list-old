071b5c921e6c169e68467c7c30fe7437b4e25a72
Jake Galligan <galligan1996@gmail.com>
2019-07-03 18:25:37 -0400

    Cleaning up preloaded React files, as well as setting a boiler plate template for my App.js file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <Main />
+    <Header />
+  </div>
+);
 
-export default App;
+const Main = () => (
+  <main>
+    <Switch>
+      <Route exact path='/contacts' component={Contacts} />
+      <Route exact path='/contacts/new' component={CreateContact}
+    </Switch>
+  </main>
+)
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
