1eb077991ae368a7c8c708058c70469429f84a48
snpdolan <snpdolan@gmail.com>
2019-07-03 14:10:54 -0400

    created general sketch of the App component, with constructor, addContact function, and render.

---
+++ src/components/App.js
@@ -0,0 +1,39 @@
+import { Switch, Route } from 'react-router-dom'
+import React from 'react'
+import Home from './Home'
+import ContactList from './ContactList'
+import AddContact from './AddContact'
+
+class App extends React.Component {
+  constructor () {
+    super()
+
+    this.state = {
+      contacts: []
+    }
+    
+    this.addContact = this.addContact.bind(this);
+  }
+  
+  addContact (contact) {
+    this.setState({contacts: this.state.contacts.concat([contact])});
+    console.log(this.state.contacts);
+  }
+  
+
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route path='/contacts' render={() => (
+            <ContactList editContact={this.editContact} contacts={this.state.contacts} />
+          )}/>
+          <Route path='/new' component={AddContact}/>
+        </Switch>
+      </div>
+    )
+  }
+}
+
+export default App
