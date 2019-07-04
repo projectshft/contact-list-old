ea30fd941f9cfe598d382a3810da5ba0acbc241b
dposse <dposse.125@gmail.com>
2019-07-04 16:28:40 -0400

    Successfully found current contact in contacts array using Route's prop.match.id

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,10 +4,19 @@ import _ from 'lodash';
 import { PropTypes } from 'prop-types';
 
 const Contact = ({props, contacts, editContact}) => {
- 
+  const currentContact = _.find(contacts, { id: +props.match.params.id });
+
+  if (!currentContact)
+    return (
+      <div>
+        <p>Sorry, contact was not found.</p>
+        <Link to='/roster'>Back</Link>
+      </div>
+    )
 
   return (
     <div>
+      {currentContact.name}
       <Link to='/contacts'>Back</Link>
     </div>
   );
