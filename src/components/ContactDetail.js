5f338c3eae95091144beb2d223c6e31caf0b3793
snpdolan <snpdolan@gmail.com>
2019-07-04 11:57:08 -0400

    updated contact cards look and amended Detail to use Link tags everywhere.

--- src/components/ContactDetail.js
+++ src/components/ContactDetail.js
@@ -21,8 +21,8 @@ const ContactDetail = ({props, contacts}) => {
         <div className="card-body">
           <h5 className="card-title">{contact.name}</h5>
           <p className="card-text">{contact.phone_number}<br></br>{contact.email}</p>
-          <span><a href='/contacts' className="btn btn-primary">Back</a>
-          <a href={`/contacts/${contact.id}/edit`} className="btn btn-secondary">Edit</a></span>
+          <span><Link to='/contacts' className="btn btn-primary">Back To Contacts</Link>
+          <Link  to={`/contacts/${contact.id}/edit`} className="btn btn-secondary">Edit</Link></span>
         </div>
       </div>
     </div>
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,16 +1,22 @@
-import { Switch, Route, Link } from 'react-router-dom'
-import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom';
+import React from 'react';
+import blank_contact from '../blank_profile_male.jpg';
 
 const ContactList = ({contacts}) => (
   <div className='col-md-8 offset-md-2'>
-    <div className="jumbotron"><h1>Contacts!</h1></div>
+    <div className="jumbotron"><h1>Rolodex</h1></div>
+    <div className='contacts-info'>
+      <span><h1><em>Total Contacts: {contacts.length}</em></h1></span>
+      <Link className='btn btn-secondary btn-lg btn-block align-right' to={`/contacts/new`}>Add Contact</Link>
+    </div>
+    
 
-    <Link className='btn btn-secondary' to={`/contacts/new`}>Add Contact</Link>
-    <ul>
+    <ul className='list-group'>
       {
         contacts.map(c => (
-          <li key={c.id}>
-            <Link to={`/contacts/${c.id}`}>{c.name}</Link> <Link to={`/contacts/${c.id}/edit`}>Edit</Link>
+          <li key={c.id} className='list-group-item'>
+            <img className="float-left" src={c.image_url === '' ? blank_contact: c.image_url}></img>
+            <Link to={`/contacts/${c.id}`}>{c.name}</Link> <Link className='btn btn-outline-info btn-md float-right aligh-middle' to={`/contacts/${c.id}/edit`}>Edit</Link>
           </li>
         ))
       }
