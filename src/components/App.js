94a4034825304381d17018054fc7c10da64cdf9f
Jake Galligan <galligan1996@gmail.com>
2019-07-04 09:00:23 -0400

    Reformatting the layout of my index.js fild to include the rendering of the app

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+class App extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      contactList = {
+        [{
+          'id': 1 'name': 'Sir PumperNickle Junior'
+          'image_url': 'https://pixel.nymag.com/imgs/daily/vulture/2017/11/27/27-lil-pump.w700.h700.jpg'
+          'email': 'lilPump@gmail.com'
+          'phone_number': '305-274-2834'
+        }]
+      }
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <Switch>
+          <Route exact path='/contacts' component={Contacts}/>
+          <Route exact path='/contact/new' component={NewContact} />
+        </Switch>
+      </div>
+    )
+  }
+}
+
+export default App
