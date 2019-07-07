d79804ad70d96ddcf99cd2a99f6a596eaf3a1dda
K. Bender <kristinajbender@gmail.com>
2019-07-07 19:55:24 -0400

    Add form validation to ContactEdit (to match ContactNew)

--- src/components/ContactEdit.js
+++ src/components/ContactEdit.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
 import { sendEvent } from './State';
 import PropTypes from 'prop-types';
 
@@ -17,6 +17,7 @@ class ContactEdit extends React.Component {
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleEdit = this.handleEdit.bind(this);
+    this.handleValidation = this.handleValidation.bind(this);
   }
 
   // Listen for changes to input values and update state accordingly. 
@@ -24,19 +25,50 @@ class ContactEdit extends React.Component {
     this.setState({ [event.target.id]: event.target.value });
   }
 
+  // Same function as in ContactNew (see comments there). 
+  handleValidation() {
+    const errors = [];
+    if (this.state.name === '') {
+      errors.push('You must enter a name.');
+    }
+    if (this.state.email !== '') {
+      let lastAtPos = this.state.email.lastIndexOf('@');
+      let lastDotPos = this.state.email.lastIndexOf('.');
+      if (!(lastAtPos < lastDotPos
+        && lastAtPos > 0
+        && this.state.email.indexOf('@@') === -1
+        && lastDotPos > 2
+        && (this.state.email.length - lastDotPos) > 2)) {
+        errors.push('If you include an email address, it must be valid.');
+      }
+    }
+    if (this.state.phone_number !== '') {
+      if (!this.state.phone_number.match(/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/)) {
+        errors.push('If you include a phone number, it must be valid.')
+      }
+    }
+    return errors.join(' ');
+  }
+
   // Handle edits to contact (save changes). Redirect to main page on submit. 
+  // (Alert with error message if any errors are found.)
   handleEdit(event) {
     event.preventDefault();
-    const updatedContact = this.state;
-    sendEvent('editContact', updatedContact);
-    this.props.history.push('/');
+    const error = this.handleValidation();
+    if (!error) {
+      const updatedContact = this.state;
+      sendEvent('editContact', updatedContact);
+      this.props.history.push('/');
+    } else {
+      alert(error);
+    }
   }
 
   // Component receives only the contact that matches the params id. 
   // Set state with contact so form is pre-filled. 
   componentDidMount() {
-    const contact = this.props.contact; 
-    this.setState({...contact});  
+    const contact = this.props.contact;
+    this.setState({ ...contact });
   }
 
   render() {
@@ -46,7 +78,7 @@ class ContactEdit extends React.Component {
           Edit Contact
         </div>
         <div>
-        <button className="btn btn-light"><Link to='/'>&laquo; Back to Contacts</Link></button>
+          <button className="btn btn-light"><Link to='/'>&laquo; Back to Contacts</Link></button>
         </div>
         <form onSubmit={this.handleEdit} className="input-form">
           <input className="form-control" type="text" id="name" value={this.state.name} onChange={this.handleChange} placeholder="Name" />
@@ -62,12 +94,12 @@ class ContactEdit extends React.Component {
 
 ContactEdit.propTypes = {
   contact: PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      name: PropTypes.string.isRequired,
-      email: PropTypes.string.isRequired,
-      phone_number: PropTypes.string.isRequired,
-      image_url: PropTypes.string.isRequired
-    }).isRequired
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    phone_number: PropTypes.string.isRequired,
+    image_url: PropTypes.string.isRequired
+  }).isRequired
 };
 
 export default ContactEdit; 
