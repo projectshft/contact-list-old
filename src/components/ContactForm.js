6033f81db65977c5e3979cbf9f78fbfd6272aee5
dposse <dposse.125@gmail.com>
2019-07-07 18:59:21 -0400

    Added ContactForm plan

---
+++ src/components/ContactForm.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+/**
+ * ContactForm can handle all the logic that NewContact and EditContact are handling
+ * Make New/EditContact pure components that pass state and functions to ContactForm
+ * 
+ * NewContact only receives addContact function
+ * EditContact receives router props, contacts array, editContact function
+ */
+
+
+class ContactForm extends Component {
+  
+}
+
+export default ContactForm;
