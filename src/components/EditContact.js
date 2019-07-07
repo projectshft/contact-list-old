fd3dee2f160e481b619959c97e8ce521ed5834ee
dposse <dposse.125@gmail.com>
2019-07-07 18:42:54 -0400

    Refactored state object in EditContact so it matches state object in NewContact

--- src/components/EditContact.js
+++ src/components/EditContact.js
@@ -6,9 +6,10 @@ class EditContact extends Component {
   constructor(props) {
     super(props);
 
-    this.state={
-      currentContact: _.find(this.props.contacts, {id: +this.props.props.match.params.id})
-    };
+    //destructuring so the state object matches state object in NewContact component
+    // it is one Contact object
+    // if you change this, change NewContact as well so forms work the same for both components
+    this.state={..._.find(this.props.contacts, {id: +this.props.props.match.params.id})};
     
     //handle submission by calling app's edit contact function
     //needs to be bound
@@ -16,7 +17,7 @@ class EditContact extends Component {
   }
 
   handleEditContactClick() {
-    console.log('gotem');
+    
   }
 
   render() {
@@ -24,28 +25,28 @@ class EditContact extends Component {
       <div>
         <form>
           <label>Name</label>
-          <input type='text' className='form-control' placeholder={this.state.currentContact.name}
+          <input type='text' className='form-control' placeholder={this.state.name}
           onChange={event =>
             this.setState({ name: event.target.value })
           } />
           <label>Email</label>
-          <input type='text' className='form-control' placeholder={this.state.currentContact.email}
+          <input type='text' className='form-control' placeholder={this.state.email}
           onChange={event =>
             this.setState({ email: event.target.value })
           } />
           <label>Phone Number</label>
-          <input type='text' className='form-control' placeholder={this.state.currentContact.phoneNumber}
+          <input type='text' className='form-control' placeholder={this.state.phoneNumber}
           onChange={event =>
             this.setState({ phoneNumber: event.target.value })
           } />
           <label>Image source - you can leave this blank</label>
-          <input type='text' className='form-control' placeholder={this.state.currentContact.image}
+          <input type='text' className='form-control' placeholder={this.state.image}
           onChange={event =>
             this.setState({ image: event.target.value })
           }/>
           <button type='button' className='btn btn-primary' onClick={this.handleEditContactClick}>Edit Contact</button>
         </form>
-        <Link to={`/contacts/${this.state.currentContact.id}`}>Cancel</Link>
+        <Link to={`/contacts/${this.state.id}`}>Cancel</Link>
       </div>
     )
   }
