6c05f3445c6c68762be73d1bf91bed3630044bf5
daveman235 <awad.david@gmail.com>
2019-10-17 19:23:55 -0400

    jsx template on front page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <div class="row">
+            <div class="col-8">
+              <h1 className="App-title">Contact List</h1>
+            </div>
+            <button type="button" className="btn btn-primary">Add New Contact</button>
+          </div>
         </p>
+
+
+        <div>
+          <div className="contact-list">
+            <ul className="list-group">
+              <li className="list-group-item">Tom Englestain</li>
+              <li className="list-group-item">Bob Odenkirk</li>
+              <li className="list-group-item">Frida Mendoza</li>
+              <li className="list-group-item">Bobby Babba</li>
+              <li className="list-group-item">Franky the fish</li>
+            </ul>
+          </div>
+         </div>
+
+         
       </div>
     );
   }
