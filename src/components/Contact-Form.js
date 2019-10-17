4e856f8b144bda0e5955267a3df96aaaefcad480
Alexandra Ferdinand <alexxkf@gmail.com>
2019-10-17 15:00:15 -0400

    initial commit

---
+++ src/components/Contact-Form.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class ContactForm extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      id: ,
+      name: '',
+      img: 'url',
+      email: '',
+      number:
+    }
+
+
+  }
---
+++ src/components/Contact-List.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Contact from './Contact';
+
+
+class ContactList extends React.Component {
+  render () {
+    return <div>TODO</div>;
+  }
+}
