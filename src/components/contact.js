cc613e4b0979b6ad227a5575463d4efb69a06abb
Tyler <laorik1@live.com>
2019-10-17 18:45:19 -0400

    importing lodash. updated contact component to display actual contact data from page's contact

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,7 +1,24 @@
 import React from "react";
+import _ from "lodash";
+import { Link } from "react-router-dom";
 
-const Contact = props => {
-  return <div>THIS IS THE CONTACT VIEW</div>;
+const Contact = ({ props, contacts }) => {
+  //url contains id from contact object, use that to grab viewed contact
+  const contact = _.find(contacts, {
+    id: parseInt(props.match.params.id, 10)
+  });
+
+  return (
+    <div>
+      Name: {contact.name}
+      <br />
+      Email: {contact.email}
+      <br />
+      Phone: {contact.phone_number}
+      <br />
+      <Link to="/contacts">Back</Link>
+    </div>
+  );
 };
 
 export default Contact;
