e061ec2d7ac10372ce6f45879ee8f5c24497561a
Alexandra Ferdinand <alexxkf@gmail.com>
2019-10-17 15:38:43 -0400

    setting up contact form html, added button.

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,18 @@ import ContactList from './Contact-List';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Contact List</h1>
-        </header>
-        <p className="App-intro">
-          Modern day rolodex, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <div className="page-header">
+            <h1>Contact List</h1>
+          </div>
+
+          <div className="contacts">
+          </div>
+
+          <ContactForm />
+
+        </div>
       </div>
     );
   }
--- src/components/Contact-Form.js
+++ src/components/Contact-Form.js
@@ -1,18 +1,57 @@
 import React, { Component } from 'react';
 
 class ContactForm extends Component {
-  constructor () {
+  constructor() {
     super()
 
     this.state = {
-      id: ,
+      id: 0,
       name: '',
       img: 'url',
       email: '',
-      number:
+      number: 0
     }
 
 
   }
 
-  export default ContactForm;
+  render() {
+    return (
+      <form>
+        <h3>Add a New Post</h3>
+
+        <div className="form-group">
+          <input
+            className="form-control"
+            placeholder="Name"
+             />
+
+          <br/>
+
+          <input
+            className="form-control"
+            placeholder="Picture"
+            />
+
+    <br/>
+        <input
+          className="form-control"
+          placeholder="Email"
+          />
+    <br/>
+
+      <input
+        className="form-control"
+        placeholder="Phone Number"
+        
+        />
+    </div>
+
+        <button onClick={this.handleClick} type="button" className="btn btn-primary add-post">Add Contact</button>
+      </form>
+    );
+  }
+}
+
+
+export default ContactForm;
