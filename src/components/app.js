53ae237ed9511c461be1d470fc9bccf935e41f7d
Tyler <laorik1@live.com>
2019-10-17 17:22:08 -0400

    moved hard coded list of contact data out of contact-list and into a state setup on app. sending contact data from app to contact-list via props

--- src/components/app.js
+++ src/components/app.js
@@ -2,11 +2,36 @@ import React, { Component } from "react";
 import Header from "./header";
 import Main from "./main";
 
-const App = () => (
-  <div>
-    <Header />
-    <Main />
-  </div>
-);
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      contacts: [
+        {
+          id: 70219577,
+          name: "Albert Einstein",
+          image_url:
+            "https://en.wikipedia.org/wiki/Albert_Einstein#/media/File:Einstein_1921_by_F_Schmutzer_-_restoration.jpg",
+          email: "aeinstein@example.com",
+          phone_number: "15555555555"
+        }
+      ]
+    };
+
+    this.addContact = this.addContact.bind(this);
+  }
+
+  addContact(contact) {
+    this.setState({ posts: this.state.contacts.concat([contact]) });
+  }
+
+  render = () => (
+    <div>
+      <Header />
+      <Main contacts={this.state.contacts} />
+    </div>
+  );
+}
 
 export default App;
--- src/components/main.js
+++ src/components/main.js
@@ -3,15 +3,21 @@ import React from "react";
 import ContactAddForm from "./contact-add-form";
 import ContactList from "./contact-list";
 
-const Main = () => (
-  <main>
-    <Switch>
-      <Redirect exact from="/" to="/contacts" />
-      {/* /contacts/add first since regular /contacts causes a match to contacts/add */}
-      <Route path="/contacts/add" component={ContactAddForm} />
-      <Route path="/contacts" component={ContactList} />
-    </Switch>
-  </main>
-);
+const Main = props => {
+  console.log(props);
+  return (
+    <main>
+      <Switch>
+        <Redirect exact from="/" to="/contacts" />
+        {/* /contacts/add first since regular /contacts causes a match to contacts/add */}
+        <Route path="/contacts/add" component={ContactAddForm} />
+        <Route
+          path="/contacts"
+          render={() => <ContactList contacts={props.contacts} />}
+        />
+      </Switch>
+    </main>
+  );
+};
 
 export default Main;
