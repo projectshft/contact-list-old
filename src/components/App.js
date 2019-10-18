6eb9cd467c2aac2da4dae142443c0997b316cf5b
George Green <greezy@Georges-MacBook-Air.local>
2019-10-18 13:22:55 -0400

    Hard-coded contact rendering to the page.

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,16 @@ class App extends Component {
   constructor() {
     super()
 
-  }
+  this.state = {
+    contacts: [
+    { ID: 1, name: "Ben Blocker", imageURL: "G", email: 'test@123.com', phone: '123-456-7890' },
+    { ID: 2, name: "John Jacobs", imageURL: "G", email: 'test@123.com', phone: '123-456-7890' }
+  ],
+};
+console.log(this.state.contacts[0].name);
+
+}
+
 
   render() {
     return (<div className="container-fluid">
@@ -20,6 +29,11 @@ class App extends Component {
           <hr/>
         <div className="contacts-list">
           <h1>Contact</h1>
+        <div className="contact1">
+        <h2>Name: { this.state.contacts[0].name } </h2>
+      <button type="button" className="btn btn-primary btn-sm">Edit</button>
+    <button type="button" className="btn btn-secondary btn-sm">Delete</button>
+        </div>
         </div>
         </div>
       </div>
