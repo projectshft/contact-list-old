5ef52c2516169866ed5503605a8c0193efad7fa0
Billy DeLashmutt <delashmuttwa@gmail.com>
2019-10-19 16:48:14 -0400

    Remove unnecessary code for routing to contact details

--- src/components/App.js
+++ src/components/App.js
@@ -9,18 +9,12 @@ class App extends Component {
     super()
 
     this.state = {
-      currentContact: null,
       contacts: 
       [
         {id: '123', name: 'Testnasasdasme', email: "testemail.gmail.com", phone: "5556667777", image: ''},
         {id: '456', name: 'Othertsadaest name thing', email: "fake123.gmail.com", phone: "5553334444", image: 'https://tekrabuilders.com/wp-content/uploads/2018/12/male-placeholder-image-300x300.jpeg'}
       ]
     }
-  }
-
-  // Sets currentContact state which, in turn, triggers a conditional to render contact details
-  setCurrentContact = contactId => {
-    this.setState({ currentContact: contactId})
   }
 
   // Adds contacts to end of contacts array in state
