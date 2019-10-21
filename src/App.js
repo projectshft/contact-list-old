129b45fcec167c792b1b0a3588881bd35e43a4fc
Patrick Crawford <53802272+pjcrawford@users.noreply.github.com>
2019-10-21 08:48:42 -0400

    commit 2
    commit 2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Table from './Table';
+import Form from './Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//import logo from './logo.svg';
+//import './App.css';
+
+// Contact list eval skeleton!!
+
+//Contacts component is the Main.  it will include
+  // 1.) add contact button
+  // 2.) "Contacts" Header
+  // 3.) Name sub-Component
+          // a) edit + delete buttons
+          // b) Contact Info sub-Component
+                  // clicking the Contact Info will be the same as edit?
+
+//start with contacts component and build from there
+
+//test 2
 
 class App extends Component {
+  state = {
+      characters: []
+  };
+
+  removeCharacter = index => {
+      const { characters } = this.state;
+  
+      this.setState({
+          characters: characters.filter((character, i) => { 
+              return i !== index;
+          })
+      });
+  }
+
+  handleSubmit = character => {
+      this.setState({characters: [...this.state.characters, character]});
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      const { characters } = this.state;
+      
+      return (
+          <div className="container">
+              <h1>React Contacts</h1>
+              <Table
+                  characterData={characters}
+                  removeCharacter={this.removeCharacter}
+              />
+              <h3>Add New</h3>
+              <Form handleSubmit={this.handleSubmit} />
+          </div>
+      );
   }
 }
-
+//layout update needed? for revision?
 export default App;
