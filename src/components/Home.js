9eace3392fffab88e4b147182af9ed9370f49c26
Patrick Crawford <53802272+pjcrawford@users.noreply.github.com>
2019-11-06 10:08:17 -0500

    added button on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Home = () => (
   <div>
     <h1>Patrick's Contact List</h1>
-  </div>
+    
+      <button className='btn'>
+          <Link to='/contacts/new'>Add Contact</Link></button>
+          </div>
+   
 )
 
 export default Home
