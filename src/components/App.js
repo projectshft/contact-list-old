9a154a375b6308c148b9d724e4e82216fcc0dc56
Patrick Crawford <53802272+pjcrawford@users.noreply.github.com>
2019-11-22 09:44:48 -0500

    fixed compiling, MVP is working, css not included yet

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import { Switch, Route } from 'react-router-dom'
-import React, { Component } from 'react';
+import React from 'react';
 import Home from './Home'
-import ListofContacts from './ListofContacts'
+import ListofContacts from './Listofcontacts'
 import ContactNew from './ContactNew'
 import ContactDetail from './ContactDetail'
 
@@ -12,7 +12,7 @@ class App extends React.Component {
     this.state = {
       contacts: [
         //my contact to populate on load
-        { id:1000000000, name: "Patrick", imgUrl:"https://media.licdn.com/dms/image/C4E03AQHpEkQPqrVkWw/profile-displayphoto-shrink_200_200/0?e=1578528000&v=beta&t=Vo6fjMxNdUGva_HOrs9o8Ptlu-uVoj25pBJL6RwYd4s", email: "pcrawford94@gmail.com", number: "9194132003"},
+        { id:1000000000, name: "Patrick", email: "pcrawford94@gmail.com", number: "9194132003"},
       ]
     }
     
