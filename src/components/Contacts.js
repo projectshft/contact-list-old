34edb12eda76d251e6d87f421d46fabb460d585f
Tatiana Grigorieva <tatianagrigorieva@Tatianas-MacBook-Pro.local>
2020-02-14 08:01:21 -0500

    added new contact functionality

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -7,10 +7,10 @@ import ContactList from './ContactList'
 
 export default function Contacts(props) {
 
-  let sample = [
-        { number: 1, name: "Ben Blocker", phone: "444-333-2211", email: "test@gmail.com", img_url: " empty " },
-        { number: 2, name: "Tim Smith", phone: "444-333-2211", email: "test@gmail.com", img_url: " empty " },
-      ]
+  	let sample = [
+        {  name: "Ben Blocker", phone: "444-333-2211", email: "test@gmail.com", photo: " empty " },
+        { name: "Tim Smith", phone: "444-333-2211", email: "test@gmail.com", photo: " empty " },
+     ]
 	const [contacts, setContacts] = useState(sample);
   // const [name, setName] = useState('Mary')
   // const [surname, setSurname] = useState('Poppins')
@@ -34,25 +34,27 @@ export default function Contacts(props) {
   //   setSurname(e.target.value)
   // }
 
-  return (
-    <div>
-        <Switch>
-          <Route path="/contacts/add">
-              <NewContact />
-          </Route>
-          <Route path="/contacts/update">
-              <UpdateContactInfo />
-          </Route>
-          <Route path="/contacts/2">
-              <Contact />
-          </Route>
-          <Route path='/contacts' render={() => (
+
+
+	return (
+	<div>
+	    <Switch>
+	      <Route path="/contacts/add" render={(routerProps) => (
+			<NewContact  contacts={contacts} />
+			)}/>
+	      <Route path="/contacts/update">
+	          <UpdateContactInfo />
+	      </Route>
+	      <Route path="/contacts/2">
+	          <Contact />
+	      </Route>
+	      <Route path='/contacts' render={() => (
 			<ContactList contacts={contacts} />
 			)}/>
-        </Switch>
+	    </Switch>
 
-    </div>
-  )
+	</div>
+	)
 };
 
 // class Contacts extends React.Component {
--- src/components/NewContact.js
+++ src/components/NewContact.js
@@ -1,16 +1,95 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
-class NewContact extends React.Component {
-  render() {
-    return (
+export default function NewContact(props, contacts) {
+
+	const [contact, setContactDetails] = useState({name: '', phone: '', email: '', photo: ''});
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		console.log('submit clicked')
+		console.log('from handleSubmit contact: ', contact)
+		if (!contact) return;
+		addContact(contact)
+	}
+
+	const handleInputChange = e => {
+        const {name, value} = e.target
+        setContactDetails({...contact, [name]: value})   
+    }
+
+	const addContact = contact => {
+		console.log('from addContact: ', contact)
+		props.contacts.push(contact);
+		console.log('contacts array after added contact: ', props.contacts)
+	};
+
+	return (
       <div>
-      	New Contact
-      	<div><Link to='/contacts'>Back</Link></div>
+        <form onSubmit={handleSubmit}>
+	        <label>Name</label>
+			<input
+				type="text"
+				name="name"
+				className="input"
+
+				onChange={handleInputChange}
+			/>
+
+	        <br/>
+
+	        <label>Phone</label>
+			<input
+				type="text"
+				name="phone"
+				className="input"
+
+				onChange={handleInputChange}
+			/>
+
+	        <br/>
+
+	        <label>Email</label>
+			<input
+				type="text"
+				name="email"
+				className="input"
+
+				onChange={handleInputChange}
+			/>
+
+	        <label>Photo</label>
+			<input
+				type="text"
+				name="photo"
+				className="input"
+
+				onChange={handleInputChange}
+			/>
+	        <button type="submit" >Submit</button>
+        </form>
+
+        <Link to='/contacts'>Contact List</Link>
       </div>
 
-    )
-  }
-}
 
-export default NewContact;
+  );
+
+
+};
+
+
+
+// class NewContact extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//       	New Contact
+//       	<div><Link to='/contacts'>Back</Link></div>
+//       </div>
+
+//     )
+//   }
+// }
+
+// export default NewContact;
