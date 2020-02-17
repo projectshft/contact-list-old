4fb869c3ac3402cf5671815bb823f046e9cb79e6
BENZABOSS <joyceben33@gmail.com>
2020-02-16 22:46:49 -0500

    Adedd pre-filled data to the state.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,20 @@
 import { Switch, Route } from 'react-router-dom';
 import React, { Component } from 'react'
+import uuid from 'react-uuid';
+
 import Home from './Home'
 import Contacts from './Contacts'
 
 class App extends Component {
   constructor() {
     super()
-
+    // uuid invoked generates a random id number
     this.state = {
       people: [
-        { id: 1, name: "Ben Joyce", imgURL: "www.https:", email: "bmjoyce@ncsu.edu", phone_number: '3364550205' }
+        { id: uuid(), name: "Jimbo Dixon", email: "jdixon@gmail.com", phone_number: '3361887913', imgURL: "https://randomuser.me/api/portraits/men/3.jpg" },
+        { id: uuid(), name: "Zach Kring", email: "zkring@gmail.com", phone_number: '3364568210', imgURL: "https://randomuser.me/api/portraits/men/63.jpg" },
+        { id: uuid(), name: "Dana Cox", email: "danaCox@gmail.com", phone_number: '336456888', imgURL: "https://randomuser.me/api/portraits/women/21.jpg" },
+        { id: uuid(), name: "Kate Spade", email: "katespade@gmail.com", phone_number: '336456999', imgURL: "https://randomuser.me/api/portraits/women/69.jpg" }
       ]
     }
 
@@ -26,7 +31,9 @@ class App extends Component {
       <div>
         <Switch>
           <Route exact path='/' component={Home} />
-          <Route path='/contacts' component={Contacts} />
+          <Route path='/contacts' render {(routerProps) => (
+            <Contacts people={this.state.people} />
+          )} />
           
         </Switch>
       </div>
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,10 +2,12 @@ import { Link } from 'react-router-dom';
 import React from 'react';
 
 
-const Contacts = () => (
+
+const Contacts = ({ people }) => (
     <div>
         <Link to="/" >Home</Link>
         <h1 className="text-center" >Contacts</h1>
+
     </div>
 )
 
