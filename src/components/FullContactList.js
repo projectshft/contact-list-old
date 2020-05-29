fbbc4d10c889e45ae37750fa941d8b2ad2d259ba
MCola <mcolapietro502@gmail.com>
2020-05-29 17:31:47 -0400

    added fullcontactlist to map all contacts to /contacts route

---
+++ src/components/FullContactList.js
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom'
+import React from 'react'
+
+const FullContactList = ({contacts}) => (
+  <div>
+    <ul>
+      {
+        contacts.map(contact => (
+          <li key={contact.number}>
+            <Link to={`/contact/${contact.number}`}>{contact.name}</Link>
+          </li>
+        ))
+      }
+    </ul>
+  </div>
+)
+
+export default FullRoster
