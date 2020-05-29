01ba852f87dda86dbedeb7b8422e610c74574f15
Joe Krall <kralljr@outlook.com>
2020-05-29 15:26:17 -0400

    'initial'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
