03bbdbde2a80d72828c5ceae832c1f6cfd25a066
MCola <mcolapietro502@gmail.com>
2020-05-30 16:23:07 -0400

    added form for user to enter info to add a new contact

--- src/components/ContactNew.js
+++ src/components/ContactNew.js
@@ -1,14 +1,49 @@
-import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import React from 'react';
 
-class ContactNew extends Component {
+class ContactNew extends React.Component {
   constructor() {
     super()
+
+    this.state = {
+      id: null,
+      name: '',
+      phoneNumber: '',
+      email: ''
+    }
   }
   
+
   
     render() {
       return (
-        <div>ToDo</div>
+        <div>
+          {/* form to hold the values the user will submit to add a new contact */}
+          <form>
+            <label>Name</label>
+            {/* updating the state with the value of the input */}
+            <input type='text' className='form-control' onChange={event => this.setState({ name: event.target.value })
+            }/>
+
+            <br/>
+
+            <label>Phone Number</label>
+            {/* changing the phone number input to a text to add to the state */}
+            <input type='text' className='form-control' onChange={event => this.setState({ phoneNumber: parseInt(event.target.value, 10) })
+            }/>
+
+            <br/>
+
+            <label>Email</label>
+            <input type='text' className='form-control' onChange={event => this.setState({ email: event.target.value })
+            }/>
+            {/* button to handle the start of the function that will pass 
+            the new contact info to App.js */}
+            <button type="button" onClick={this.handleSubmitNewContact}>Submit</button>
+
+          </form>
+
+        </div>
       );
     }
 }
