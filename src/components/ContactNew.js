994d044e24c4e127a14076bc2bd6bb59fd723f43
Spalding Vance <spaldingvance@Spaldings-MacBook-Pro.local>
2020-05-30 14:08:35 -0400

    built out form for ContactNew.js

--- src/components/ContactNew.js
+++ src/components/ContactNew.js
@@ -18,13 +18,33 @@ class ContactNew extends React.Component {
 
   render () {
     return (
-      <form>
-        <div className="form-group">
-          <label for="email-input">Email Address</label>
-          <input type="email" className="form-control"/>
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h1>Add a Contact</h1>
+          <form>
+            <div className="form-group">
+              <label for="name-input">Name</label>
+              <input type="text" id="name-input" className="form-control"/>
+            </div>
+            <div className="form-group">
+              <label for="email-input">Email Address</label>
+              <input type="email" id="email-input" className="form-control"/>
+            </div>
+            <div className="form-group">
+              <label for="phone-input">Phone Number</label>
+              <input type="text" id="phone-input" className="form-control"/>
+            </div>
+            <div className="form-group">
+              <label for="img-input">Profile Image URL</label>
+              <input type="text" id="img-input" className="form-control"/>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
+          <button type="button" className="btn btn-warning">
+            <Link to='/contacts'>Contacts</Link>
+          </button>
         </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
-      </form>
+      </div>
 
     )
   }
