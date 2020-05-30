a7a55d71177445234409648e4d5c0be5e35dacc4
MCola <mcolapietro502@gmail.com>
2020-05-30 16:34:30 -0400

    added functionality for the contact to be pass state to App.js componenet when a user submits new info

--- src/components/ContactNew.js
+++ src/components/ContactNew.js
@@ -11,41 +11,58 @@ class ContactNew extends React.Component {
       phoneNumber: '',
       email: ''
     }
-  }
+
+    this.handleSubmitNewContact = this.handleSubmitNewContact.bind(this);
+  };
   
+  handleSubmitNewContact () {
+    const generateId = () => Math.round(Math.random() * 100000000)
 
-  
-    render() {
-      return (
-        <div>
-          {/* form to hold the values the user will submit to add a new contact */}
-          <form>
-            <label>Name</label>
-            {/* updating the state with the value of the input */}
-            <input type='text' className='form-control' onChange={event => this.setState({ name: event.target.value })
-            }/>
-
-            <br/>
-
-            <label>Phone Number</label>
-            {/* changing the phone number input to a text to add to the state */}
-            <input type='text' className='form-control' onChange={event => this.setState({ phoneNumber: parseInt(event.target.value, 10) })
-            }/>
-
-            <br/>
-
-            <label>Email</label>
-            <input type='text' className='form-control' onChange={event => this.setState({ email: event.target.value })
-            }/>
-            {/* button to handle the start of the function that will pass 
-            the new contact info to App.js */}
-            <button type="button" onClick={this.handleSubmitNewContact}>Submit</button>
-
-          </form>
-
-        </div>
-      );
+    const newContact = {
+      id: generateId(),
+      name: this.state.name,
+      phoneNumber: this.state.phoneNumber,
+      email: this.state.email
     }
+
+    this.props.addContact(newContact);
+    this.props.history.push('/contacts');
+  };
+  
+  render() {
+    return (
+      <div>
+        {/* form to hold the values the user will submit to add a new contact */}
+        <form>
+          <label>Name</label>
+          {/* updating the state with the value of the input */}
+          <input type='text' className='form-control' onChange={event => this.setState({ name: event.target.value })
+          }/>
+
+          <br/>
+
+          <label>Phone Number</label>
+          {/* changing the phone number input to a text to add to the state */}
+          <input type='text' className='form-control' onChange={event => this.setState({ phoneNumber: parseInt(event.target.value, 10) })
+          }/>
+
+          <br/>
+
+          <label>Email</label>
+          <input type='text' className='form-control' onChange={event => this.setState({ email: event.target.value })
+          }/>
+          {/* button to handle the start of the function that will pass 
+          the new contact info to App.js */}
+          <button type="button" onClick={this.handleSubmitNewContact}>Submit</button>
+
+        </form>
+        
+        <br/>
+
+        <Link to='/contacts'>Back to Contacts List</Link>
+      </div>
+    );
+  }
 }
 
 export default ContactNew;
