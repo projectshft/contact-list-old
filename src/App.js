f70a8cf380aa6d37d211d88e78007e343ac6b912
Clyde Weed <clydeweed@gmail.com>
2020-05-31 21:44:46 -0400

    Added hard-coded contacts to App.js for testing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// testing hardcoded contacts
+
+let contacts = [
+  {
+    "id": 70219577,
+    "name": "Albert Einstein",
+    "image_url": "https://en.wikipedia.org/wiki/Albert_Einstein#/media/File:Einstein_1921_by_F_Schmutzer_-_restoration.jpg",
+    "email": "aeinstein@example.com",
+    "phone_number": "15555555555"
+  },
+  {
+    "id": 70219578,
+    "name": "Homer Simpson",
+    "image_url": "https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png",
+    "email": "homer@compuglobalhypermeganet.com",
+    "phone_number": "9395550113"
+  },
+  {
+    "id": 70219579,
+    "name": "Ghostbusters Inc.",
+    "image_url": "https://upload.wikimedia.org/wikipedia/en/e/ee/Ghostbusters_logo.svg",
+    "email": "pvenkmann@ghostbusters.com",
+    "phone_number": "1234567890"
+  }
+]
+
+
+// Creating a simple div header
 
 class App extends Component {
   render() {
