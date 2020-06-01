d22a30a43b4789fb45ac8fb5926ae7d63279f233
Joe Krall <kralljr@outlook.com>
2020-05-31 20:17:58 -0400

    added additional boilerplate contacts

--- src/components/App.js
+++ src/components/App.js
@@ -13,10 +13,24 @@ class App extends React.Component {
       contacts: [
         {
           id: 70219574,
-          name: "Albert Einstein I",
+          name: "Albert Einstein",
           image_url: "https://upload.wikimedia.org/wikipedia/en/thumb/8/86/Einstein_tongue.jpg/220px-Einstein_tongue.jpg",
-          email: "aeinstein1@example.com",
-          phone_number: "15555555555"
+          email: "albert@einstein.me",
+          phone_number: "19625897078"
+        },
+        {
+          id: 234509875,
+          name: "John Flansburgh",
+          image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTX4n9TtWuV2cmUTpEOYJXGoFOKcij_DmndUlrjxt_RaaivFm_v&usqp=CAU",
+          email: "john.flansburgh@tmbg.net",
+          phone_number: "14235079991"
+        },
+        {
+          id: 46584567764,
+          name: "Merry Clayton",
+          image_url: "https://hys2d3ayt6x6b7ts22ypg68p-wpengine.netdna-ssl.com/wp-content/uploads/2013/07/Merry-Clatyon-Photo-Courtesy-of-C-Lou-Adler-Ode-Sounds-Visuals.jpg",
+          email: "merry.clayton@outlook.com",
+          phone_number: "14367890324"
         }
       ]
     }
