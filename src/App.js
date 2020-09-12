2dd08bffd63724b8f245092a3484bcf12a237647
Daniel Renteria <d.renteria_a@yahoo.com>
2020-09-12 12:47:49 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
+//Show user list of contacts
+//Allow user to add a contact with name, email, phone number
+//Use proptypes
+//Clicking 'add contact' takes user to new route (/contacts/new)
+    //shows form for adding new contact
+    //after clicking submit user is routed to /contacts
+//include edit/delete buttons
+//From '/contacts' each contact should be clickable
+    //clicking contact navigates to (/contacts/{contact id}) 
+//Generate id for each new contact
+//Individual contact view should include a "back" button
+
+
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
       </div>
     );
   }
