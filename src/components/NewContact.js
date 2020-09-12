26acb01b0382dd33cbde1f6c7ec1846ea994ad86
Clark Hollar <clarkhollar@Clarks-MacBook-Pro.local>
2020-09-12 11:10:10 -0400

    updates new contact component to hard code in a form that will eventually update our contact list

--- src/components/NewContact.js
+++ src/components/NewContact.js
@@ -2,9 +2,32 @@
 // the route /contacts/new , once submit button is clicked should rerout back to /contacts
 // with that new contact appearing 
 import React from 'react';
-
+// adds a new contact component that will have input fields for a new contact to be added to our contat list on 
+// home page
 const NewContact = () => (
-    <div></div>
+    <div>
+        <form>
+        <label>Name</label>
+        <input type='text' className='form-control'/>
+
+        <br/>
+
+        <label>Image URL</label>
+        <input type='text' className='form-control'/>
+
+        <br/>
+
+        <label>Phone #</label>
+        <input type='text' className='form-control'/>
+
+        <br/>
+
+        <label>Email</label>
+        <input type='text' className='form-control'/>
+
+        <button type="button" onClick={this.handleSubmitContactClick}>Submit</button>
+        </form>
+    </div>
 )
 
 
