73a12e3d339ae71f7ea0e1830bd2bdb6443e2f2a
Clark Hollar <clarkhollar@Clarks-MacBook-Pro.local>
2020-09-12 11:56:02 -0400

    adds a clickable link on the /contacts page that routes to the contacts/new page

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,1 +1,11 @@
 // component that will serve as button to route to the /contacts/new
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const AddContact = () => (
+    <div>
+        <Link to='/contacts/new'>Add Contact</Link>
+    </div>
+)
+
+export default AddContact
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,12 @@
 // as well as the add new contact component 
 import React from 'react'
 import ContactList from './ContactList'
+import AddContact from './AddContact'
 
 const Home = () => (
     <div>
         <h1>Contacts</h1>
-        <button>Add Contact</button>
+        <AddContact />
         <ContactList />
     </div>
     
