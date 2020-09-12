1ce1bbba28d50a80212deb0c18ab0b2665c66f94
Clark Hollar <clarkhollar@Clarks-MacBook-Pro.local>
2020-09-12 13:22:23 -0400

    adds state to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,27 @@ import NewContact from './NewContact'
 
 
 class App extends React.Component {
+  constructor () {
+    super ()
+
+    this.state = {
+      contacts: [
+        {
+          id: 70219577,
+          name: "Albert Einstein",
+          image_url: "https://en.wikipedia.org/wiki/Albert_Einstein#/media/File:Einstein_1921_by_F_Schmutzer_-_restoration.jpg",
+          email: "aeinstein@example.com",
+          phone_number: 15555555555
+        }
+      ]
+    }
+
+    this.addContact = this.addContact.bind(this);
+  }
+  // create a function that will take the inputs from the new contact form and add the valueses to the contact list
+  addContact (contact) {
+    this.setState({contacts: this.state.contacts.concat([contact])});
+  }
   render() {
     return (
       <div>
