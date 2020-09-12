3805cadaac286b874968db0e600cd35dc6c35b43
PJ Foley <pjfoley@gmail.com>
2020-09-12 16:34:23 -0700

    Basic structure plotted out. Not compiling yet

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ContactsList from './ContactsList';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App container">
+        <div className="row">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <div className="col-md">
+           <h1 className="App-title float-left">Contacts</h1>
+           <button type="button" className="btn float-right btn-outline-dark shadow add-contact ml-5 mt-2">Add Contact</button>
+          </div>
         </header>
+        </div>
+        <div className="row mt-3">
+          <div className="col-md">
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Contacts go here
+          <ContactsList contacts={contacts}/>
         </p>
+        </div>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+
+ReactDOM.render(
+  //<BrowserRouter>   // for future construction with router
+    <App />, document.getElementById('root'));
+  //</BrowserRouter>
+
+
